c8fad0e268cdce6c54cda5fc664e204c881a56d2
hoilaptrinhgame <dominogames.net@gmail.com>
2019-08-11 00:41:03 +0700

    @override deleted

--- ContentProvider/app/src/main/java/com/tutorialspoint/contentprovider/MainActivity.java
+++ ContentProvider/app/src/main/java/com/tutorialspoint/contentprovider/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 import android.widget.ImageView;
 public class MainActivity extends Activity {
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
